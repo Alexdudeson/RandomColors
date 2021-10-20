ac9493d55bb05b01217d426ef5fa2c3f18e33cf3
Alexey Yarov <alexxd91@gmail.com>
2021-10-20 17:07:00 +0300

    Initial Commit

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by Alexey Yarov on 20/10/2021.
+//  Copyright © 2021 Alexey Yarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
